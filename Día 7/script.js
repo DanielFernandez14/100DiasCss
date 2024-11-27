2529dd884c8aa0b4eac9f6e1d0800b49da1cd7e6
DanielFernandez14 <daniel.mfernandez1999@gmail.com>
2024-11-27 18:55:48 -0300

    Dia 7

---
+++ Día 7/script.js
@@ -0,0 +1,15 @@
+const buscar= document.querySelector(".bx.bx-search");
+const menu= document.querySelector(".menu-icon");
+
+let buscadore = document.querySelector("search");
+let note = document.querySelector(".note");
+let listas = document.querySelector(".lista");
+
+buscar.addEventListener("click", ()=>{
+    buscadore.classList.toggle("input");
+});
+
+menu.addEventListener("click", ()=>{
+    note.classList.toggle("active");
+    listas.classList.toggle("activo");
+})
